3c81241822546456ff5651ad9bd4548e27559562
Sean G Trettel <sean_trettel@brown.edu>
2018-11-16 12:22:14 -0500

    Completed addition of two-tone test
    Added '2 Tone Test' condition to scaredyRat-GEN, also includes generalized epoch functions which can use, but don't require, Ethovision nest labels.

--- src/scaredyratfunctions.py
+++ src/scaredyratfunctions.py
@@ -4,13 +4,13 @@ import time
 import numpy as np
 import pandas as pd
 
-def find_epoch(df, epochLabel, isFlagged, numEpochs, epochStartTime, epochEndTime, window):
+def find_epoch(df, epochLabel, isFlagged, numEpochs, epochStartTime, epochEndTime):
     '''
     Creates dictionary of each epoch. Values are
     dataframes...
     '''
-    tones = {}
-    i = 0
+    epochs = {}
+    i = 1
     while i <= numEpochs: # number of epochs
         if(isFlagged): # Ethovision has a bucket for this epoch type
             num = str(i)
@@ -28,10 +28,35 @@ def find_epoch(df, epochLabel, isFlagged, numEpochs, epochStartTime, epochEndTim
             
             epochs[i] = epoch #Store the epoch in the output structure
         else:
-            itime = df.index.get_loc(epochStartTime,method='bfill') #Index for pretone start
-            etime = df.index.get_loc(epochEndTime,method='bfill') #Index for pretone end
+            itime = df.index.get_loc(epochStartTime[i-1],method='bfill') #Index for pretone start
+            etime = df.index.get_loc(epochEndTime[i-1],method='bfill') #Index for pretone end
             epochs[i] = df.iloc[itime:etime]
         
         i += 1
         
     return(epochs)
+    
+def find_epoch_vels(df, numEpochs):
+    '''
+    '''
+    epochVels = {}
+    i=1
+    while i <= numEpochs:
+        num = str(i)
+        epochDF = []
+        epochstart = df[i].iloc[0] # Time for epoch start
+        epochend = df[i].iloc[-1] #Time for epoch end
+        #print(epochstart)
+        starttime = math.floor(epochstart['Recording time']) #Time for epoch start
+        endtime = math.floor(epochend['Recording time']) #Time for epoch end
+        itime = df[i].index.get_loc(starttime,method='bfill') #Index for epoch start
+        etime = df[i].index.get_loc(endtime,method='bfill') #Index for epoch end
+    
+        epochDF = df[i].iloc[itime:etime] #df for epoch i
+        epochVels[i] = epochDF['Velocity']
+        i+=1
+    return(epochVels)
+
+#def find_epoch_freezing(df, numEpochs, thresh):
+    
+#def find_epoch_darting(df, numEpochs, thresh):
